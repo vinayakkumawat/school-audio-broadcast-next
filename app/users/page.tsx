cca9deaeb54161838543aba4eedfa651606789ac
Vinayak Kumawat <iamvinayakkt@gmail.com>
2024-12-30 13:55:11 +0530

    users page heading update

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,6 @@ const UsersPage = () => {
     return (
         <Layout>
             <div className="space-y-6">
-                <h1 className="text-2xl font-bold text-gray-900">User Management</h1>
                 <UserManagement />
             </div>
         </Layout>
